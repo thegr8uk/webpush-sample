70fa34f1eef8c21b18a0e5178649858a57f9d8a2
Nikhil K <knikhil@opentext.com>
2025-06-15 11:48:26 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
 self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', options);
+    options = {
+        body: 'This is a notification from your service worker!',
+        icon: 'icon.png',
+        badge: 'badge.png',
+        vibrate: [100, 50, 100],
+        data: {
+            url: 'https://example.com'
+        }
+    };
+    self.registration.showNotification('Hello world!', options);
 });
