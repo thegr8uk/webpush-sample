58f79030eb0864782c93a90fe95e7de51b5a01cb
Nikhil K <knikhil@opentext.com>
2025-06-15 11:57:23 +0530

    fix for taking msg from backend'

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-self.addEventListener('push', () => {
-    options = {
-        body: 'This is a notification from your service worker!',
-        icon: 'icon.png',
-        badge: 'badge.png',
-        vibrate: [100, 50, 100],
-        data: {
-            url: 'https://example.com'
-        }
-    };
-    self.registration.showNotification('Hello world!', options);
+self.addEventListener('push', (event) => {
+  let data = {};
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  const title = data.title || 'Default Title';
+  const options = {
+    body: data.body || 'Default body content',
+    icon: 'icon.png',
+    badge: 'badge.png',
+    vibrate: [100, 50, 100],
+    data: {
+      url: data.url || 'https://example.com'
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
 });
--- webpush.js
+++ webpush.js
@@ -11,6 +11,12 @@ push.setVapidDetails(
   vapidKeys.privateKey
 );
 
-let subscription = { "endpoint": "https://fcm.googleapis.com/fcm/send/eRSLQ19eKEI:APA91bFhVUiTfHYYW5Fa-2xCF0z6dfoNc132bNskWmzt-wOVlbUukPoZdI15NOQW4jJXQvYaoFxYHdxi92AmviSnhaC-GiNsllvX_vCUvxDoqlfdTFeC7YFYRrpr60WA5CkocpidMDcF", "expirationTime": null, "keys": { "p256dh": "BMzqVru4hyTFCFVMYngUCO78UmixKrBvVBxqKI4NiiN4GtgasUuRWcyuP5_OoeulmKT1eGVSNQIDZWb9L1sF-OA", "auth": "FBcuXLRz2es68KQLPAIJWA" } };
+let subscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/eRSLQ19eKEI:APA91bFhVUiTfHYYW5Fa-2xCF0z6dfoNc132bNskWmzt-wOVlbUukPoZdI15NOQW4jJXQvYaoFxYHdxi92AmviSnhaC-GiNsllvX_vCUvxDoqlfdTFeC7YFYRrpr60WA5CkocpidMDcF","expirationTime":null,"keys":{"p256dh":"BMzqVru4hyTFCFVMYngUCO78UmixKrBvVBxqKI4NiiN4GtgasUuRWcyuP5_OoeulmKT1eGVSNQIDZWb9L1sF-OA","auth":"FBcuXLRz2es68KQLPAIJWA"}};
 
-push.sendNotification(subscription, 'Your push notification message here');
+const payload = JSON.stringify({
+  title: 'Push from Backend',
+  body: 'Your push notification message here',
+  url: 'https://yourdomain.com/page'
+});
+
+push.sendNotification(subscription, payload);
